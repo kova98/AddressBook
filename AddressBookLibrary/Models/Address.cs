8a5b3d7e93f4a8b0b9ceb6dbdc3031fce4028d05
Roko Kovač <rokokovac98@gmail.com>
2018-09-19 14:39:13 +0200

    Add person, group, address model

---
+++ AddressBookLibrary/Models/Address.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace AddressBookLibrary.Models
+{
+    /// <summary>
+    /// Contains full address information. 
+    /// </summary>
+    class Address
+    {
+        /// <summary>
+        /// The ID of the address. Used as the primary key.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The street name.
+        /// </summary>
+        public string StreetName { get; set; }
+
+        /// <summary>
+        /// The street number.
+        /// </summary>
+        public string StreetNumber { get; set; }
+
+        /// <summary>
+        /// The city.
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// The state.
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// The country.
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// The zip code.
+        /// </summary>
+        public int ZipCode { get; set; }
+    }
+}
---
+++ AddressBookLibrary/Models/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLibrary.Models
+{
+    /// <summary>
+    /// Contains full information of a person in the address book.
+    /// </summary>
+    class Person
+    {
+        /// <summary>
+        /// The ID of the person. Used as the primary key.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The person's first name.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The person's last name.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The person's phone number.
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// The person's address.
+        /// </summary>
+        public Address Address { get; set; }
+    }
+}
